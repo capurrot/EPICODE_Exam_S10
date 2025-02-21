0a09f6d6f6bedbb9bf0f37ebc86fe76f2e891762
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-02-21 15:23:43 +0000

    logo responsive

--- src/component/LogoComponent.jsx
+++ src/component/LogoComponent.jsx
@@ -2,7 +2,12 @@ import { Image } from "react-bootstrap";
 import logo from "../assets/logo.png";
 
 const LogoComponent = () => {
-  return <Image src={logo} width={150} className="p-3 position-absolute mb-3" />;
+  return (
+    <>
+      <Image src={logo} width={150} className="p-3 d-none d-md-block position-absolute mb-3" />
+      <Image src={logo} width={150} className="p-3 d-flex mx-auto d-md-none mb-3" />
+    </>
+  );
 };
 
 export default LogoComponent;
